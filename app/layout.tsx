02fae732e1f157d8026d371e4921c9a33ece29b3
Krzychu PC <zalewastriker10@gmail.com>
2024-03-02 12:31:44 +0100

    02.03.2024 - favicon + meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Ukraina from "@/components/Ukraina";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Auto-S OSK",
+  title: "Auto-S OSK - Szkoła jazdy w Skórzewie",
   description: "Szkoła jazdy Poznań, Skórzewo, Dopiewo, Plewiska i okolice",
 };
 
@@ -19,6 +19,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="max-w-[1440px] mx-auto">
+      <meta
+        name="author"
+        content="Ośrodek Szkolenia Kierowców AUTO-S - Sławomir Pługowski"
+      ></meta>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <meta
+        property="og:title"
+        content="Auto-S OSK - Szkoła Nauki Jazdy w Skórzewie, Plewiskach, Przeźmierowie, Poznaniu i innych miejscowościach"
+      ></meta>
+      <meta
+        property="og:description"
+        content="Profesjonalna szkoła nauki jazdy w Skórzewie, Plewiskach, Przeźmierowie, Poznaniu i okolicach. Badania lekarskie w cenie, elastyczny grafik, własny plac manewrowy. Zapisz się już dziś!"
+      ></meta>
+      <meta
+        name="keywords"
+        content="nauka jazdy, prawo jazdy,szkola jazdy, szkoła jazdy poznań, Skórzewo, Plewiska, Przeźmierowo, Poznań, Szkoła Nauki Jazdy, kurs prawa jazdy, badania lekarskie, plac manewrowy"
+      ></meta>
       <meta
         name="google-site-verification"
         content="lChrf4R4smlYnl2HUIpK_azfQoVLO3ts4bzBtBrEGAw"
